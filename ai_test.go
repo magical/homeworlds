466a2818f221696dfb244a9b7fe8a169135e4ef6
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2017-06-18 17:20:51 -0700

    integrate sacrifice actions into minimax
    this probably isn't as efficient as it could be, but it's a start.
    
    note that i had to disable Y3 sacrifices because the search space was
    too huge.
    
    also: fix an accounting error when gc'ing a catastrophe at a homeworld.
    the game is over at that point, so it shouldn't matter, but it was
    causing false positives in the sanity check.

--- ai_test.go
+++ ai_test.go
@@ -34,7 +34,7 @@ func TestSacrificeActions(t *testing.T) {
 	size = uintptr(len(acts)) * unsafe.Sizeof(acts[0])
 	for _, sa := range acts {
 		fmt.Println(sa)
-		size += uintptr(len(sa.Actions)) * unsafe.Sizeof(sa.Actions[0])
+		size += uintptr(sa.N()) * unsafe.Sizeof(sa.Action(0))
 	}
 	fmt.Println("size:", size)
 	fmt.Println("len:", len(acts))
@@ -60,6 +60,6 @@ func BenchmarkMinimax(b *testing.B) {
 	pos := PositionFromGame(game)
 	for i := 0; i < b.N; i++ {
 		ai := NewAI()
-		ai.Minimax(pos, Action{})
+		ai.Minimax(pos, BasicAction{})
 	}
 }
